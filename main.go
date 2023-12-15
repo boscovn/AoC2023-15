2490d012aa7b665324fc43d64ef78660cc259c1d
Bosco Vallejo-Nágera Esteban <juan.vallejonagera@newryglobal.com>
2023-12-15 12:03:53 +0100

    Part two done

--- main.go
+++ main.go
@@ -17,6 +17,11 @@ func runHash(text string) int {
 	return cur
 }
 
+type lens struct {
+	length   int
+	position int
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	if !scanner.Scan() {
@@ -24,9 +29,50 @@ func main() {
 	}
 	text := scanner.Text()
 	s := strings.Split(text, ",")
-	sum := 0
+	boxes := make(map[int]map[string]lens)
 	for _, v := range s {
-		sum += runHash(v)
+		last := v[len(v)-1]
+		if last == '-' {
+			l := v[:len(v)-1]
+			h := runHash(l)
+			box, ok := boxes[h]
+			if ok {
+				lense, ok := box[l]
+				if ok {
+					o := lense.position
+					delete(box, l)
+					for k, v := range box {
+						if v.position > o {
+							v.position--
+							box[k] = v
+						}
+					}
+				}
+			}
+
+		} else {
+			l := v[:len(v)-2]
+			h := runHash(l)
+			box, ok := boxes[h]
+			if ok {
+				lense, ok := box[l]
+				if ok {
+					box[l] = lens{position: lense.position, length: int(last - '0')}
+				} else {
+					box[l] = lens{position: len(box), length: int(last - '0')}
+				}
+			} else {
+				lenses := make(map[string]lens)
+				lenses[l] = lens{position: 0, length: int(last - '0')}
+				boxes[h] = lenses
+			}
+		}
+	}
+	sum := 0
+	for k, v := range boxes {
+		for _, v := range v {
+			sum += (k + 1) * v.length * (v.position + 1)
+		}
 	}
 	fmt.Println(sum)
 }
